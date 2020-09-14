1772d7d16a62d67799578234273f09c1b3012413
kishibe-rohan <shahnawaz.21810589@viit.ac.in>
2020-09-14 22:44:01 +0530

    Updates

---
+++ precache-manifest.dc4e258c7f335da0dfc281a098cc8301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27e5bcede639b03d841",
+    "url": "/reactmdb-movie-app/static/js/runtime~main.be58ccf1.js"
+  },
+  {
+    "revision": "5aec6e6659e5918cba17",
+    "url": "/reactmdb-movie-app/static/js/main.14b2ae05.chunk.js"
+  },
+  {
+    "revision": "d37c4bb92d5cd72ce4f1",
+    "url": "/reactmdb-movie-app/static/js/2.df6b6195.chunk.js"
+  },
+  {
+    "revision": "5aec6e6659e5918cba17",
+    "url": "/reactmdb-movie-app/static/css/main.788685e9.chunk.css"
+  },
+  {
+    "revision": "1a5061b0e6e7973c707047afc8a255e6",
+    "url": "/reactmdb-movie-app/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/reactmdb-movie-app/precache-manifest.dc4e258c7f335da0dfc281a098cc8301.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/reactmdb-movie-app/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
